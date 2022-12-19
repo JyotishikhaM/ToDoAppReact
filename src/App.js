1f5bf23eb301ba5c8326167c2d7e6c81cf42b775
JyotishikhaM <114400013+JyotishikhaM@users.noreply.github.com>
2022-12-19 11:18:41 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react'; 
+import Header from './Header';
+import data from './data.json'
+import ToDoList from './ToDoList'
+import ToDoForm from './ToDoForm'
+
+
+function App() {
+  const[todoList,setToDoList]=useState(data);
+  function handleToggle(id)
+  {
+    let mapped = todoList.map(task => {
+      return task.id === id ? {...task, complete: !task.complete } : {...task};
+    });
+    setToDoList(mapped);
+  }
+  function handleFilter()
+  {
+    let filtered = todoList.filter(task => {
+      return !task.complete;
+    });
+    setToDoList(filtered);
+  } 
+  function addTask(userInput)
+  {
+    let copy=[...todoList];
+    copy = [...copy, { id: todoList.length + 1, task: userInput, complete: false }];
+    setToDoList(copy);    
+  }
+   
+    
+  return (
+    <div className="App">
+      <Header/>
+      <ToDoList todoList={todoList} handleToggle={handleToggle} handleFilter={handleFilter}/>
+      <ToDoForm addTask={addTask}/>
+    </div>
+  );
+}
+
+export default App;
